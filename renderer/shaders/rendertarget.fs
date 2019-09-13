e1892b522d03781d49d0d08a73fe7c9c188a45bf
gpu.cruncher@gmail.com <gpu.cruncher@gmail.com>
2019-09-13 09:32:22 +0530

    added general comments

--- renderer/shaders/rendertarget.fs
+++ renderer/shaders/rendertarget.fs
@@ -6,5 +6,9 @@ uniform sampler2D u_RT1_tex;
 
 void main()
 {
+	// the original
 	gl_FragColor = texture( u_RT1_tex, fs_uv );
+	
+	//1st sept debugging black screen
+	//gl_FragColor = vec4(1,0,0,1);
 }
